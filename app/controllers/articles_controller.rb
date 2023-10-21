068592bd06fe3d4e4ec3fb48ac38ae95814fa687
anakettl <anakettl@gmail.com>
2023-10-21 17:29:16 -0300

    feat(article): cria novo serializer para article list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,19 @@ class ArticlesController < ApplicationController
     render json: { dep: article }, status: 200
   end
 
+  def index
+    articles = Article.all
+
+    render json: serializer.new(articles), status: 200
+  end
+
   private
 
   def article_params
     params.require(:articles).permit(:name)
+  end
+
+  def serializer
+    ArticleSerializer
   end
 end
--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe '/articles routes' do
     expect(get '/articles').to route_to('articles#index')
     expect(get 'articles?page[number]=3').to route_to('articles#index', page: { number: 3 })
   end
+
+  it 'returns a proper JSON' do
+    article = create :article
+    get '/articles'
+    body = JSON.parse(response.body)
+    expect(body).to eq(
+      data: [
+        {
+          id: article.id,
+          type: 'articles',
+          attributes: {
+            title: article.title,
+            content: article.content,
+            slug: article.slug
+          }
+        }
+      ]
+    )
+  end
 end
